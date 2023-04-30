5226ac409b03ed162ea138d586ff32df9e5d4216
agaley <agaley@student.42lyon.fr>
2023-04-30 02:27:08 +0200

    First draft

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 00:10:16 by agaley            #+#    #+#             */
-/*   Updated: 2023/03/11 01:56:06 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/04/15 19:27:50 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,38 @@ static int	ft_error(int code)
 
 int	main(int argc, char **argv)
 {
-	int	i;
+	char	bin[9];
+	size_t	i;
+	size_t	j;
 
 	if (argc != 3 || !argv[2][0])
 		return (ft_error(1));
 	ft_printf("%d arguments :\n", argc - 1);
-	i = 1;
-	while (i < argc)
-		ft_printf("%s\n", argv[i++]);
+	i = 0;
+	j = 0;
+	while (i < ft_strlen(argv[2]))
+	{
+		ft_itobarr((unsigned char)argv[2][i], 8, bin);
+		while (j < 8)
+		{
+			if (bin[j] == '0')
+				kill(ft_atoi(argv[1]), SIGUSR1);
+			else
+				kill(ft_atoi(argv[1]), SIGUSR2);
+			j++;
+			usleep(100);
+		}
+		usleep(1000);
+		// k = 1;
+		// while (utf[j][0] == 1)
+		// {
+		// 	ft_itobarr((unsigned char)argv[2][j + k], 8, bin);
+		// 	utf[j++] = ft_btoi(bin);
+		// 	j++;
+		// }
+		i++;
+	}
+	// ft_printf("%s\n", utf);
 	// Encode and send message bits by bits
 	// Receive ok from server, exit 0 - if not, exit error 2
 	return (0);
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 00:06:21 by agaley            #+#    #+#             */
-/*   Updated: 2023/03/11 01:50:35 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/04/15 19:54:50 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,89 @@
 // • Unicode characters support!
 
 #include "minitalk.h"
+#include <time.h>
+
+static void	print_msg(unsigned char *utfbin)
+{
+	size_t			len;
+	size_t			i;
+	size_t			j;
+	unsigned char	utf[5];
+	unsigned char	bin8[9];
+
+	len = ft_ustrlen(utfbin);
+	ft_printf("len - %d\n", len);
+	i = 0;
+	while (i < 4)
+	{
+		if (8 * i > len)
+			i = 4;
+		else
+		{
+			j = 0;
+			while (j < 8)
+			{
+				bin8[j] = utfbin[8 * i + j];
+				j++;
+			}
+			ft_printf("utfbin - %s\n", bin8);
+			utf[i++] = ft_btoi(bin8);
+		}
+	}
+	printf("utf - %s\n", utf);
+}
+
+static void	handler(int signum)
+{
+	static unsigned char	utfbin[33];
+	size_t			l;
+	size_t			i;
+
+     clock_t start, end;
+     double time_taken;
+
+	l = ft_ustrlen(utfbin);
+	if (l == 32 || ((l == 8 || l == 16 || l == 24) && signum == SIGUSR1))
+	{
+		start = clock();
+		print_msg(utfbin);
+		end = clock();
+     	time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("fun() took %f seconds to execute \n", time_taken);
+		i = 0;
+		while (i < 33)
+		{
+			utfbin[i] = 0;
+			i++;
+		}
+	}
+	l = ft_ustrlen(utfbin);
+	if (signum == SIGUSR1)
+		utfbin[l] = '0';
+	else if (signum == SIGUSR2)
+		utfbin[l] = '1';
+}
+
+static void	receive_signals()
+{
+	struct sigaction sa;
+
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO; /* Restart functions if interrupted by handler */
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_printf("Sigaction configuration error");
+}
 
 int	main(void)
 {
 	ft_printf("Ready to talk with client, PID = %d\n", getpid());
 	while (1)
 	{
-		pause(); // wait for signal to reach server
-		print_msg();
-		notify_client();
+		//pause(); // wait for signal to reach server
+		receive_signals();
+		usleep(1);
+		// print_msg();
+		// notify_client();
 	}
-	return (0);
 }
